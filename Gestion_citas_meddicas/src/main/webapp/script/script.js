f451e2c8051865801bd743764ee8c5f0e08da040
DESKTOP-5TCD4T0\EQUIPO <f>
2023-03-27 21:41:16 +0200

    270323

--- Gestion_citas_meddicas/src/main/webapp/script/script.js
+++ Gestion_citas_meddicas/src/main/webapp/script/script.js
@@ -1,3 +1,39 @@
-/**
- * 
- */
+class Usuario{//id,nombre,apellido,
+	
+		
+	
+}
+
+class CitaMedica{//id,hora,idUsu,idMedico,FechaHora,NumConsulta,Centro
+	
+		
+	
+}
+
+class CentroMedico
+{
+	
+		
+	
+}
+
+class ConsultaMedica{//habitaculo
+	
+		
+	
+}
+class Medico{//id,nombre,apellido,
+	
+		
+	
+}
+
+
+
+
+
+
+
+
+
+
