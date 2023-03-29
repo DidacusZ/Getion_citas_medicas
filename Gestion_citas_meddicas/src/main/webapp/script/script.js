32a6901287d7cc5c8b815810be928b29080a712a
DESKTOP-R8M679G\Equipo <f>
2023-03-29 09:47:53 +0200

    290323

--- Gestion_citas_meddicas/src/main/webapp/script/script.js
+++ Gestion_citas_meddicas/src/main/webapp/script/script.js
@@ -1,30 +1,59 @@
-class Usuario{//id,nombre,apellido,
-	
+class Usuario //id,nombre,apellido
+{	
+	constructor(idUsu,nombre,apellido){
 		
+		this.idUsu=idUsu;
+		this.nombre=nombre;
+		this.apellido=apellido;		
+	}		
 	
 }
 
-class CitaMedica{//id,hora,idUsu,idMedico,FechaHora,NumConsulta,Centro
-	
-		
-	
-}
-
-class CentroMedico
+class CitaMedica extends Usuario //id,FechaHora,idUsu,idMedico,NumConsulta,Centro
 {
 	
+	constructor(idCita,FechaHora,idMedico,NumConsulta,idCentro,idUsu,nombre,apellido){
 		
+		this.idCita=idCita;
+		this.FechaHora=FechaHora;
+		
+		//this.idUsu=idUsu;
+		super(idUsu,nombre,apellido);
+		
+		this.idMedico=idMedico;
+		this.NumConsulta=NumConsulta;
+		this.idCentro=idCentro;	
+	}
 	
 }
 
-class ConsultaMedica{//habitaculo
+class CentroMedico// id,nobreCentro,ciudad
+{
+	
+	constructor(id,nobreCentro,ciudad,numConsultas){
+		
+		this.id=id;
+		this.nobreCentro=nobreCentro;
+		this.ciudad=ciudad;
+		this.numConsultas=numConsultas;	
+	}
+	
+}
+
+class ConsultaMedica //habitaculo
+{
 	
 		
 	
 }
 class Medico{//id,nombre,apellido,
 	
+	constructor(id,nombre,apellido){
 		
+		this.id=id;
+		this.nombre=nombre;
+		this.apellido=apellido;		
+	}
 	
 }
 
